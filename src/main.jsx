a3cffaff4e75f96ca8d43816772c998491402b1d
Ranjan Gupta <Ranjanguptajeff@gmail.com>
2023-12-11 18:22:07 +0530

    Updated Footer

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import "./footer.jsx";
+import "./Footer.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-    <footer />
+    <Footer />
   </React.StrictMode>
 );
