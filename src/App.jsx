8325eeee43123d43ab172831997cfc2dfbb74426
Ranjan Gupta <Ranjanguptajeff@gmail.com>
2023-12-11 18:17:40 +0530

    Added Files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-yellow-500">
+        <h1 className="text-3xl font-semibold text-white p-3 pl-5">Keeper</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const footer = () => {
+  return (
+    <div>
+      <p className="text-center">Copyright 2023</p>
+    </div>
+  );
+};
+
+export default footer;
